6160e75cc95e2591780fec83fc171325a77ae6dd
Ckyiu <UnsignedArduino@outlook.com>
2022-07-20 11:45:29 -0400

    Remove all code from inventory example

--- main.ts
+++ main.ts
@@ -32,10 +32,7 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (in_inventory) {
         handle_a_key_in_inventory_toolbar()
     } else {
-        item = remove_item_from_toolbar(toolbar.get_number(ToolbarNumberAttribute.SelectedIndex))
-        if (item) {
-            the_player.sayText("That " + item.get_text(ItemTextAttribute.Name) + " was delicious!", 2000, true)
-        }
+    	
     }
 })
 controller.right.onEvent(ControllerButtonEvent.Repeated, function () {
@@ -48,11 +45,6 @@ controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
         move_left_in_inventory_toolbar()
     }
 })
-function spawn_item () {
-    the_food = sprites.create(all_food._pickRandom(), SpriteKind.Food)
-    the_food.setPosition(randint(0, scene.screenWidth()), randint(0, scene.screenHeight()))
-    the_food.lifespan = 5000
-}
 function move_down_in_inventory_toolbar () {
     if (cursor_in_inventory) {
         if (inventory.get_number(InventoryNumberAttribute.SelectedIndex) < inventory.get_items().length - 8) {
@@ -74,11 +66,6 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
         move_right_in_inventory_toolbar()
     }
 })
-function give_item (idx: number) {
-    the_food = sprites.create(all_food[idx], SpriteKind.Food)
-    the_food.setPosition(the_player.x, the_player.y)
-    the_food.lifespan = 5000
-}
 function handle_a_key_in_inventory_toolbar () {
     if (cursor_in_inventory) {
         if (toolbar.get_items().length < toolbar.get_number(ToolbarNumberAttribute.MaxItems)) {
@@ -153,16 +140,6 @@ function move_right_in_inventory_toolbar () {
 }
 controller.menu.onEvent(ControllerButtonEvent.Pressed, function () {
     handle_menu_key_in_inventory_toolbar()
-})
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
-    for (let index = 0; index <= all_food.length - 1; index++) {
-        if (otherSprite.image.equals(all_food[index])) {
-            if (add_item([Inventory.create_item(all_labels[index], all_food[index])])) {
-                otherSprite.destroy()
-                break;
-            }
-        }
-    }
 })
 function handle_menu_key_in_inventory_toolbar () {
     in_inventory = !(in_inventory)
@@ -286,256 +263,13 @@ controller.left.onEvent(ControllerButtonEvent.Repeated, function () {
 })
 let last_inventory_select = 0
 let last_toolbar_select = 0
+let the_player: Sprite = null
 let inventory: Inventory.Inventory = null
 let cursor_in_inventory = false
-let the_food: Sprite = null
-let the_player: Sprite = null
 let toolbar: Inventory.Toolbar = null
 let item: Inventory.Item = null
 let in_inventory = false
-let all_labels: string[] = []
-let all_food: Image[] = []
 prepare_environment()
 make_player()
 make_inventory_toolbar()
 controller.configureRepeatEventDefaults(333, 50)
-all_food = [
-img`
-    . . . . c c c b b b b b . . . . 
-    . . c c b 4 4 4 4 4 4 b b b . . 
-    . c c 4 4 4 4 4 5 4 4 4 4 b c . 
-    . e 4 4 4 4 4 4 4 4 4 5 4 4 e . 
-    e b 4 5 4 4 5 4 4 4 4 4 4 4 b c 
-    e b 4 4 4 4 4 4 4 4 4 4 5 4 4 e 
-    e b b 4 4 4 4 4 4 4 4 4 4 4 b e 
-    . e b 4 4 4 4 4 5 4 4 4 4 b e . 
-    8 7 e e b 4 4 4 4 4 4 b e e 6 8 
-    8 7 2 e e e e e e e e e e 2 7 8 
-    e 6 6 2 2 2 2 2 2 2 2 2 2 6 c e 
-    e c 6 7 6 6 7 7 7 6 6 7 6 c c e 
-    e b e 8 8 c c 8 8 c c c 8 e b e 
-    e e b e c c e e e e e c e b e e 
-    . e e b b 4 4 4 4 4 4 4 4 e e . 
-    . . . c c c c c e e e e e . . . 
-    `,
-img`
-    . . . . . . . e c 7 . . . . . . 
-    . . . . e e e c 7 7 e e . . . . 
-    . . c e e e e c 7 e 2 2 e e . . 
-    . c e e e e e c 6 e e 2 2 2 e . 
-    . c e e e 2 e c c 2 4 5 4 2 e . 
-    c e e e 2 2 2 2 2 2 4 5 5 2 2 e 
-    c e e 2 2 2 2 2 2 2 2 4 4 2 2 e 
-    c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
-    c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
-    c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
-    c e e 2 2 2 2 2 2 2 2 2 2 4 2 e 
-    . e e e 2 2 2 2 2 2 2 2 2 4 e . 
-    . 2 e e 2 2 2 2 2 2 2 2 4 2 e . 
-    . . 2 e e 2 2 2 2 2 4 4 2 e . . 
-    . . . 2 2 e e 4 4 4 2 e e . . . 
-    . . . . . 2 2 e e e e . . . . . 
-    `,
-img`
-    4 4 4 . . 4 4 4 4 4 . . . . . . 
-    4 5 5 4 4 5 5 5 5 5 4 4 . . . . 
-    b 4 5 5 1 5 1 1 1 5 5 5 4 . . . 
-    . b 5 5 5 5 1 1 5 5 1 1 5 4 . . 
-    . b d 5 5 5 5 5 5 5 5 1 1 5 4 . 
-    b 4 5 5 5 5 5 5 5 5 5 5 1 5 4 . 
-    c d 5 5 5 5 5 5 5 5 5 5 5 5 5 4 
-    c d 4 5 5 5 5 5 5 5 5 5 5 1 5 4 
-    c 4 5 5 5 d 5 5 5 5 5 5 5 5 5 4 
-    c 4 d 5 4 5 d 5 5 5 5 5 5 5 5 4 
-    . c 4 5 5 5 5 d d d 5 5 5 5 5 b 
-    . c 4 d 5 4 5 d 4 4 d 5 5 5 4 c 
-    . . c 4 4 d 4 4 4 4 4 d d 5 d c 
-    . . . c 4 4 4 4 4 4 4 4 5 5 5 4 
-    . . . . c c b 4 4 4 b b 4 5 4 4 
-    . . . . . . c c c c c c b b 4 . 
-    `,
-img`
-    . . 2 2 b b b b b . . . . . . . 
-    . 2 b 4 4 4 4 4 4 b . . . . . . 
-    2 2 4 4 4 4 d d 4 4 b . . . . . 
-    2 b 4 4 4 4 4 4 d 4 b . . . . . 
-    2 b 4 4 4 4 4 4 4 d 4 b . . . . 
-    2 b 4 4 4 4 4 4 4 4 4 b . . . . 
-    2 b 4 4 4 4 4 4 4 4 4 e . . . . 
-    2 2 b 4 4 4 4 4 4 4 b e . . . . 
-    . 2 b b b 4 4 4 b b b e . . . . 
-    . . e b b b b b b b e e . . . . 
-    . . . e e b 4 4 b e e e b . . . 
-    . . . . . e e e e e e b d b b . 
-    . . . . . . . . . . . b 1 1 1 b 
-    . . . . . . . . . . . c 1 d d b 
-    . . . . . . . . . . . c 1 b c . 
-    . . . . . . . . . . . . c c . . 
-    `,
-img`
-    . . . . . . 2 2 2 2 . . . . . . 
-    . . . . 2 2 3 3 3 3 2 e . . . . 
-    . . . 2 3 d 1 1 d d 3 2 e . . . 
-    . . 2 3 1 d 3 3 3 d d 3 e . . . 
-    . 2 3 1 3 3 3 3 3 d 1 3 b e . . 
-    . 2 1 d 3 3 3 3 d 3 3 1 3 b b . 
-    2 3 1 d 3 3 1 1 3 3 3 1 3 4 b b 
-    2 d 3 3 d 1 3 1 3 3 3 1 3 4 4 b 
-    2 d 3 3 3 1 3 1 3 3 3 1 b 4 4 e 
-    2 d 3 3 3 1 1 3 3 3 3 1 b 4 4 e 
-    e d 3 3 3 3 d 3 3 3 d d b 4 4 e 
-    e d d 3 3 3 d 3 3 3 1 3 b 4 b e 
-    e 3 d 3 3 1 d d 3 d 1 b b e e . 
-    . e 3 1 1 d d 1 1 1 b b e e e . 
-    . . e 3 3 3 3 3 3 b e e e e . . 
-    . . . e e e e e e e e e e . . . 
-    `,
-img`
-    . . . . . . b b b b . . . . . . 
-    . . . . . . b 4 4 4 b . . . . . 
-    . . . . . . b b 4 4 4 b . . . . 
-    . . . . . b 4 b b b 4 4 b . . . 
-    . . . . b d 5 5 5 4 b 4 4 b . . 
-    . . . . b 3 2 3 5 5 4 e 4 4 b . 
-    . . . b d 2 2 2 5 7 5 4 e 4 4 e 
-    . . . b 5 3 2 3 5 5 5 5 e e e e 
-    . . b d 7 5 5 5 3 2 3 5 5 e e e 
-    . . b 5 5 5 5 5 2 2 2 5 5 d e e 
-    . b 3 2 3 5 7 5 3 2 3 5 d d e 4 
-    . b 2 2 2 5 5 5 5 5 5 d d e 4 . 
-    b d 3 2 d 5 5 5 d d d 4 4 . . . 
-    b 5 5 5 5 d d 4 4 4 4 . . . . . 
-    4 d d d 4 4 4 . . . . . . . . . 
-    4 4 4 4 . . . . . . . . . . . . 
-    `,
-img`
-    . . . . . . b b b b a a . . . . 
-    . . . . b b d d d 3 3 3 a a . . 
-    . . . b d d d 3 3 3 3 3 3 a a . 
-    . . b d d 3 3 3 3 3 3 3 3 3 a . 
-    . b 3 d 3 3 3 3 3 b 3 3 3 3 a b 
-    . b 3 3 3 3 3 a a 3 3 3 3 3 a b 
-    b 3 3 3 3 3 a a 3 3 3 3 d a 4 b 
-    b 3 3 3 3 b a 3 3 3 3 3 d a 4 b 
-    b 3 3 3 3 3 3 3 3 3 3 d a 4 4 e 
-    a 3 3 3 3 3 3 3 3 3 d a 4 4 4 e 
-    a 3 3 3 3 3 3 3 d d a 4 4 4 e . 
-    a a 3 3 3 d d d a a 4 4 4 e e . 
-    . e a a a a a a 4 4 4 4 e e . . 
-    . . e e b b 4 4 4 4 b e e . . . 
-    . . . e e e e e e e e . . . . . 
-    . . . . . . . . . . . . . . . . 
-    `,
-img`
-    . . . . . . . . . . b b b . . . 
-    . . . . . . . . b e e 3 3 b . . 
-    . . . . . . b b e 3 2 e 3 a . . 
-    . . . . b b 3 3 e 2 2 e 3 3 a . 
-    . . b b 3 3 3 3 3 e e 3 3 3 a . 
-    b b 3 3 3 3 3 3 3 3 3 3 3 3 3 a 
-    b 3 3 3 d d d d 3 3 3 3 3 d d a 
-    b b b b b b b 3 d d d d d d 3 a 
-    b d 5 5 5 5 d b b b a a a a a a 
-    b 3 d d 5 5 5 5 5 5 5 d d d d a 
-    b 3 3 3 3 3 3 d 5 5 5 d d d d a 
-    b 3 d 5 5 5 3 3 3 3 3 3 b b b a 
-    b b b 3 d 5 5 5 5 5 5 5 d d b a 
-    . . . b b b 3 d 5 5 5 5 d d 3 a 
-    . . . . . . b b b b 3 d d d b a 
-    . . . . . . . . . . b b b a a . 
-    `,
-img`
-    . . . . . 3 3 b 3 3 d d 3 3 . . 
-    . . . . 3 1 1 d 3 d 1 1 1 1 3 . 
-    . . . 3 d 1 1 1 d 1 1 1 d 3 1 3 
-    . . 3 d d 1 1 1 d d 1 1 1 3 3 3 
-    . 3 1 1 d 1 1 1 1 d d 1 1 b . . 
-    . 3 1 1 1 d 1 1 1 1 1 d 1 1 3 . 
-    . b d 1 1 1 d 1 1 1 1 1 1 1 3 . 
-    . 4 b 1 1 1 1 d d 1 1 1 1 d 3 . 
-    . 4 4 d 1 1 1 1 1 1 d d d b b . 
-    . 4 d b d 1 1 1 1 1 1 1 1 3 . . 
-    4 d d 5 b d 1 1 1 1 1 1 1 3 . . 
-    4 5 d 5 5 b b d 1 1 1 1 d 3 . . 
-    4 5 5 d 5 5 d b b b d d 3 . . . 
-    4 5 5 5 d d d d 4 4 b 3 . . . . 
-    . 4 5 5 5 4 4 4 . . . . . . . . 
-    . . 4 4 4 . . . . . . . . . . . 
-    `,
-img`
-    . . . . . . . 6 . . . . . . . . 
-    . . . . . . 8 6 6 . . . 6 8 . . 
-    . . . e e e 8 8 6 6 . 6 7 8 . . 
-    . . e 2 2 2 2 e 8 6 6 7 6 . . . 
-    . e 2 2 4 4 2 7 7 7 7 7 8 6 . . 
-    . e 2 4 4 2 6 7 7 7 6 7 6 8 8 . 
-    e 2 4 5 2 2 6 7 7 6 2 7 7 6 . . 
-    e 2 4 4 2 2 6 7 6 2 2 6 7 7 6 . 
-    e 2 4 2 2 2 6 6 2 2 2 e 7 7 6 . 
-    e 2 4 2 2 4 2 2 2 4 2 2 e 7 6 . 
-    e 2 4 2 2 2 2 2 2 2 2 2 e c 6 . 
-    e 2 2 2 2 2 2 2 4 e 2 e e c . . 
-    e e 2 e 2 2 4 2 2 e e e c . . . 
-    e e e e 2 e 2 2 e e e c . . . . 
-    e e e 2 e e c e c c c . . . . . 
-    . c c c c c c c . . . . . . . . 
-    `,
-img`
-    . . . . . . . . . . . 6 6 6 6 6 
-    . . . . . . . . . 6 6 7 7 7 7 8 
-    . . . . . . 8 8 8 7 7 8 8 6 8 8 
-    . . e e e e c 6 6 8 8 . 8 7 8 . 
-    . e 2 5 4 2 e c 8 . . . 6 7 8 . 
-    e 2 4 2 2 2 2 2 c . . . 6 7 8 . 
-    e 2 2 2 2 2 2 2 c . . . 8 6 8 . 
-    e 2 e e 2 2 2 2 e e e e c 6 8 . 
-    c 2 e e 2 2 2 2 e 2 5 4 2 c 8 . 
-    . c 2 e e e 2 e 2 4 2 2 2 2 c . 
-    . . c 2 2 2 e e 2 2 2 2 2 2 2 e 
-    . . . e c c e c 2 2 2 2 2 2 2 e 
-    . . . . . . . c 2 e e 2 2 e 2 c 
-    . . . . . . . c e e e e e e 2 c 
-    . . . . . . . . c e 2 2 2 2 c . 
-    . . . . . . . . . c c c c c . . 
-    `,
-img`
-    . . . . . . . e e e e . . . . . 
-    . . . . . e e 4 5 5 5 e e . . . 
-    . . . . e 4 5 6 2 2 7 6 6 e . . 
-    . . . e 5 6 6 7 2 2 6 4 4 4 e . 
-    . . e 5 2 2 7 6 6 4 5 5 5 5 4 . 
-    . e 5 6 2 2 8 8 5 5 5 5 5 4 5 4 
-    . e 5 6 7 7 8 5 4 5 4 5 5 5 5 4 
-    e 4 5 8 6 6 5 5 5 5 5 5 4 5 5 4 
-    e 5 c e 8 5 5 5 4 5 5 5 5 5 5 4 
-    e 5 c c e 5 4 5 5 5 4 5 5 5 e . 
-    e 5 c c 5 5 5 5 5 5 5 5 4 e . . 
-    e 5 e c 5 4 5 4 5 5 5 e e . . . 
-    e 5 e e 5 5 5 5 5 4 e . . . . . 
-    4 5 4 e 5 5 5 5 e e . . . . . . 
-    . 4 5 4 5 5 4 e . . . . . . . . 
-    . . 4 4 e e e . . . . . . . . . 
-    `
-]
-all_labels = [
-"Burger",
-"Apple",
-"Lemon",
-"Drumstick",
-"Ham",
-"Pizza",
-"Donut",
-"Cake",
-"Ice cream",
-"Strawberry",
-"Cherries",
-"Taco"
-]
-for (let index = 0; index <= all_food.length - 1; index++) {
-    for (let index2 = 0; index2 < 3; index2++) {
-        give_item(index)
-    }
-}
-game.onUpdateInterval(1000, function () {
-    spawn_item()
-})
